309c892536aba0f2eed5ca038833e91fe7fe4c29
Luis Alvarado <luis.alvaradofcb@gmail.com>
2023-05-22 20:27:21 -0400

    First commit

---
+++ Object Repository/Page_Edipro/a_Volver a Edipay.rs
@@ -0,0 +1,131 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Volver a Edipay</name>
+   <tag></tag>
+   <elementGuidId>bb4e91b1-eb51-41dd-88f3-9134fc69ff31</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[contains(text(),'Volver a Edipay')]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>a.btn.ripple.btn-outline-info.btn-rounded.btn-lg</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>00a6291f-0fe4-42b9-95a0-1925d36c9996</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>btn ripple btn-outline-info btn-rounded btn-lg</value>
+      <webElementGuid>b704ee65-c830-4f6c-a008-2835779667e1</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>/edipay</value>
+      <webElementGuid>d71f0938-03dd-48e3-92ad-5ca88e799a76</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value> Volver a Edipay</value>
+      <webElementGuid>469f1146-2dc8-45cd-8259-ff812edd5a63</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[@class=&quot;main-body&quot;]/div[@class=&quot;page&quot;]/div[@class=&quot;content&quot;]/div[@class=&quot;container-lg&quot;]/div[@class=&quot;inner-body&quot;]/div[@class=&quot;row row-sm&quot;]/div[@class=&quot;col-lg-5 col-md-12&quot;]/div[@class=&quot;d-none d-sm-none d-md-block&quot;]/div[@class=&quot;card-body&quot;]/div[1]/div[1]/div[@class=&quot;row&quot;]/div[@class=&quot;col-12 d-none d-sm-none d-md-block&quot;]/a[@class=&quot;btn ripple btn-outline-info btn-rounded btn-lg&quot;]</value>
+      <webElementGuid>5acf6768-5786-4891-a4df-eca4788b384a</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:link</name>
+      <type>Main</type>
+      <value>//a[contains(text(),'Volver a Edipay')]</value>
+      <webElementGuid>1ef96fef-b30f-4998-aa68-31855493338b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='¡Felicitaciones! Estas al día con tu cuenta.'])[1]/following::a[1]</value>
+      <webElementGuid>f381cef3-7a5d-48ff-9cbe-2e760dd46d32</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='No tienes deudas'])[1]/following::a[1]</value>
+      <webElementGuid>5366cac9-0e50-499d-aede-777ab197d914</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='¡Felicitaciones! Estas al día con tu cuenta'])[1]/preceding::a[1]</value>
+      <webElementGuid>28808a54-fc45-4bf8-a744-2f08915e7a8b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Volver a Edipay'])[2]/preceding::a[1]</value>
+      <webElementGuid>215e4220-0b24-44e6-98a4-e61db64a2595</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Volver a Edipay']/parent::*</value>
+      <webElementGuid>a43eb5b6-42e5-487e-968a-50f8c9d7080a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>//a[contains(@href, '/edipay')]</value>
+      <webElementGuid>0239f52a-a988-48b2-a766-aba8b47b9dcf</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//a</value>
+      <webElementGuid>da3c9837-3ac2-4dc9-b9a3-e05b5e4c93ce</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = '/edipay' and (text() = ' Volver a Edipay' or . = ' Volver a Edipay')]</value>
+      <webElementGuid>963f9d2a-4d1e-4bcd-b7e6-db496bc445bc</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Test Suites/TS02.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS02</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>120</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>d7fb902f-c59f-4fa5-80de-dce3b1604853</testSuiteGuid>
+   <testCaseLink>
+      <guid>9d3c2ed1-90ad-43c6-8339-d0df3773b143</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC02</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
